5b98947b2dfe5431f07c78d7d30410dbe09e8031
umair <umairliaqat552>
2022-02-21 16:20:46 +0500

    change in http as http

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,4 +1,4 @@
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'dart:convert';
 
 class NetworkHelper {
@@ -7,7 +7,7 @@ class NetworkHelper {
   final String url;
 
   Future getData() async {
-    http.Response response = await http.get(Uri.parse(url));
+    Response response = await get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       String data = response.body;
